64cdc8ed756fdc8384918fb044f8dc0f5521e7e9
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-09 09:26:21 +0900

    docs: Add docs directory for sphinx documentation page

---
+++ docs/source/conf.py
@@ -0,0 +1,23 @@
+import importlib.metadata
+
+_PROJECT_METADATA = importlib.metadata.metadata('varst')
+
+project = _PROJECT_METADATA['Name']
+author = _PROJECT_METADATA['Author']
+release = _PROJECT_METADATA['Version']
+copyright = f'2022, {project}'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. varST documentation master file, created by
+   sphinx-quickstart on Wed Nov  9 09:16:01 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to varST's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
