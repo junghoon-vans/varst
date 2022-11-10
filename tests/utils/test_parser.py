c0f97de40100edde9b777f30aa2d56f52324d219
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-10 12:08:17 +0900

    test: Add test_parse_variables function to test_parser

--- tests/utils/test_parser.py
+++ tests/utils/test_parser.py
@@ -6,15 +6,23 @@ from varst.utils.parser import Parser
 @pytest.fixture
 def parser() -> Parser:
     parser = Parser()
+    parser.parse([
+        'varst=variable to reStructuredText',
+        'version=0.2.0',
+        'release=v0.2.0',
+    ])
     return parser
 
 
-def test_parse_without_file_path(parser: Parser):
-    parser.parse([
-        'varst=variable to reStructuredText',
-    ])
+def test_parse_variables(parser: Parser):
+    assert parser.variables == {
+        'varst': 'variable to reStructuredText',
+        'version': '0.2.0',
+        'release': 'v0.2.0',
+    }
 
-    assert parser.variables == {'varst': 'variable to reStructuredText'}
+
+def test_parse_without_file_path(parser: Parser):
     assert parser.input_file == './README.rst'
     assert parser.output_file == './README.rst'
 
