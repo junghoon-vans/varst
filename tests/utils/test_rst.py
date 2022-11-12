93f9afc23c979a1bd54f222f62e517d559be7574
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-12 15:55:19 +0900

    test: Add pytest.fixture to test_rst.py

--- tests/utils/test_rst.py
+++ tests/utils/test_rst.py
@@ -1,14 +1,20 @@
 import os.path
 from pathlib import Path
 
+import pytest
 from docutils.nodes import document as Document
 
 from varst.utils.rst import RstDocument
 
 
-TEST_DATA_DIR = os.path.join(Path(__file__).resolve().parent.parent, 'data')
+@pytest.fixture
+def rst_document() -> RstDocument:
+    test_data_path = os.path.join(
+        Path(__file__).resolve().parent.parent, 'data',
+    )
+    return RstDocument(src=os.path.join(test_data_path, 'README.rst'))
 
 
-def test_get_document():
-    rst_document = RstDocument(src=os.path.join(TEST_DATA_DIR, 'README.rst'))
+def test_get_document(rst_document: RstDocument):
+
     assert isinstance(rst_document.document, Document)
