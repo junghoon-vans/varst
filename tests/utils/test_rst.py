e6ed486382b4f08a2ec37a3a1f933c805ac46128
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-14 10:16:06 +0900

    test: Add test_replace_substitution

--- tests/utils/test_rst.py
+++ tests/utils/test_rst.py
@@ -22,3 +22,8 @@ def test_get_document(rst_document: RstDocument):
 
 def test_substitution_text(rst_document: RstDocument):
     assert rst_document.substitution_text("status") == "false"
+
+
+def test_replace_substitution(rst_document: RstDocument):
+    rst_document.replace_substitution("status", "true")
+    assert rst_document.substitution_text("status") == "true"
